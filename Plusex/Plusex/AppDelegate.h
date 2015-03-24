87b0616a66feb6fa89c7be07afb9e8a9d6ed205a
chichilam <chichilam86@gmail.com>
2015-03-24 21:09:37 +0900

    app initial

---
+++ Plusex/Plusex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plusex
+//
+//  Created by rin on 2015/03/24.
+//  Copyright (c) 2015年 rin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Plusex/Plusex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Plusex
+//
+//  Created by rin on 2015/03/24.
+//  Copyright (c) 2015年 rin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
